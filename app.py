a19a69ea3650c6db23b8cbedd40a1c9dbf27e275
DenisZhurin <129519135+DenisZhurin@users.noreply.github.com>
2023-06-03 16:59:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -127,10 +127,10 @@ with st.container() as koef_sopr_trenia:
         var_sin = st.selectbox("Угол раскрытия диффузора (α,градусы)", (5, 10, 30, 60))
         var_D2 = st.number_input("Диаметр после диффузора (D2, )")
     with func8:
-        drob = simplify(latex(d/D2))
-#         kvadr = latex((1 - drob**2)**2)
-#         f = latex(3.2*Kд*kvadr*tan_sympy(d/2)**1.25)
-        st.write(drob)
+        drob = simplify(d/D2)
+        kvadr = simplify((1 - drob**2)**2)
+        f = simplify(3.2*Kд*kvadr*tan_sympy(d/2)**1.25)
+        st.write(f)
     with res8:
         st.write("Ответ:")
         if var_D2:
