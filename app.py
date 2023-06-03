f2546a4b4453da72bed912b2606ab740791438d7
DenisZhurin <129519135+DenisZhurin@users.noreply.github.com>
2023-06-03 17:57:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -173,11 +173,11 @@ with st.container() as soprot_konfuz:
     with space10:
         print()
     with func10:
-        slog_1 = simplify(-0.0125 * (d/D1)**8 + 0.0224*(d/D1)**6 - 0.00723(d/D1)**4 + 0.00444(d/D1)**2 - 0.000745)
-#         slog_2 = simplify((0.01745* α)**3 - 2*pi*(0.01745*α)**2 - 10*0.01745*α)
+#         slog_1 = simplify(-0.0125 * (d/D1)**8 + 0.0224*(d/D1)**6 - 0.00723(d/D1)**4 + 0.00444(d/D1)**2 - 0.000745)
+        slog_2 = simplify((0.01745* α)**3 - 2*pi*(0.01745*α)**2 - 10*0.01745*α)
 #         slog_3 = ξтр
-        f = simplify(slog_1 * slog_2 + slog_3)
-        st.write(slog_1)
+#         f = simplify(slog_1 * slog_2 + slog_3)
+        st.write(slog_2)
     with res10:
         st.write("Ответ:")
 #         if var_D2:
